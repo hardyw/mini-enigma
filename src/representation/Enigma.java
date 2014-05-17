317f0e298b6a7f5fb262e3e6d021cf403988f940
hardy <hardy_w@yahoo.com>
2014-05-17 15:45:13 -0700

    Enigma code tidied up

--- src/representation/Enigma.java
+++ src/representation/Enigma.java
@@ -1,11 +1,16 @@
 package representation;
 
+import utility.Utility;
+
 public class Enigma {
 
-	private Rotor rotL;
-	private Rotor rotM;
-	private Rotor rotR;
+	//Constants to define the rotors
+	private static final int LEFT = 0;
+	private static final int MID = 1;
+	private static final int RIGHT = 2;
+
 	
+	private Rotor[] rotors = new Rotor[3];	
 	private char[] rotorKnockers;
 	
 	private Reflector reflector;
@@ -14,20 +19,19 @@ public class Enigma {
 	private RingSetting ringSetting;
 
 	
-	public Enigma(Rotor[] rotors, Reflector ref, Plugboard plug, RingSetting rings, WindowSetting initWindow) {
+	
+	
+	public Enigma(Rotor[] rots, Reflector ref, Plugboard plug, RingSetting rings, WindowSetting initWindow) {
 	
 
-		//Following WW2 Convention, it is Left-Mid-Right
-		this.rotL = rotors[0];
-		this.rotM = rotors[1];
-		this.rotR = rotors[2];
-		this.rotorKnockers = new char[] {rotL.getKnocker(), rotM.getKnocker(), rotR.getKnocker() };
+		//Following WW2 Convention, it is specified Left-Mid-Right
+		this.rotors = rots;
+		this.rotorKnockers = new char[] {rotors[LEFT].getKnocker(), rotors[MID].getKnocker(), rotors[RIGHT].getKnocker() };
 		
 		this.reflector = ref;
 		this.plugboard = plug;
 		this.windowSetting = initWindow;
 		this.ringSetting = rings;
-		
 		
 		
 	}
@@ -50,7 +54,7 @@ public class Enigma {
 			
 			if (!Character.isWhitespace(in)) {
 
-				if (!Character.isLetter(in)) { //if input is not between 'A' and 'Z'
+				if (!Character.isLetter(in)) {
 					System.out.println("!!INVALID INPUT; NOT CHARACTER!!");
 				}
 				
@@ -58,12 +62,15 @@ public class Enigma {
 				//But the actual encrypt/decrypt happens from Right->Mid->Left
 				
 				
+				//result holds the intermediary values as well as the final output
+				char result;
+				
 				
 				/* ***********************
 				 * PLUGBOARD TRANSLATION
 				 * ***********************/
 				
-				char plug_out = plugboard.translate(in);
+				result = plugboard.translate(in);
 				
 				
 				
@@ -74,96 +81,58 @@ public class Enigma {
 				// TICK ROTOR BEFORE ENCRYPTING
 				tick();
 
-				// These are a few variables we will keep using during rotor stages
-				int dispRingAboveSea;			//displacement of Ring with respect to global 'A' position
-				int dispRotorInternalWrtRing;
-				int dispRotorInternalAboveSea;	//always > 0, expressed in mod26
+				// variables we will keep using during rotor stages
+				int offsetRotInternalAboveSea;	//always > 0, expressed in mod26
 				int inputValueRotorInternal;	//always > 0, expressed in mod26
 				
-				// RIGHT ROTOR
-				dispRingAboveSea = 'A' - windowSetting.getWindowValueRight(); //if window shows 'C', disp = -2
-				dispRotorInternalWrtRing = ringSetting.rightOffset(); //offset = 4 means internal A is locked to ring's E
-				dispRotorInternalAboveSea = (dispRingAboveSea + dispRotorInternalWrtRing + 26) % 26;			
-				inputValueRotorInternal = ((plug_out - 'A') - dispRotorInternalAboveSea + 26) % 26;
 				
-				char rotR_internalInput = (char) ('A' + inputValueRotorInternal);			
-				char rotR_internalOutput = rotR.process(rotR_internalInput);
-				char rotR_sealevelOutput = (char) ('A' + 
-										((rotR_internalOutput - 'A') + dispRotorInternalAboveSea) % 26);
-				
-				// MIDDLE ROTOR
-				dispRingAboveSea = 'A' - windowSetting.getWindowValueMid();
-				dispRotorInternalWrtRing = ringSetting.midOffset();
-				dispRotorInternalAboveSea = (dispRingAboveSea + dispRotorInternalWrtRing + 26) % 26;			
-				inputValueRotorInternal = ((rotR_sealevelOutput - 'A') - dispRotorInternalAboveSea + 26) % 26;
-				
-				char rotM_internalInput = (char) ('A' + inputValueRotorInternal);
-				char rotM_internalOutput = rotM.process(rotM_internalInput);
-				char rotM_sealevelOutput = (char) ('A' + 
-										((rotM_internalOutput - 'A') + dispRotorInternalAboveSea) % 26);
-				
-				// LEFT ROTOR
-				dispRingAboveSea = 'A' - windowSetting.getWindowValueLeft();
-				dispRotorInternalWrtRing = ringSetting.leftOffset();
-				dispRotorInternalAboveSea = (dispRingAboveSea + dispRotorInternalWrtRing + 26) % 26;
-				inputValueRotorInternal = ((rotM_sealevelOutput - 'A') - dispRotorInternalAboveSea + 26) % 26;
-				
-				char rotL_internalInput = (char) ('A' + inputValueRotorInternal);
-				char rotL_internalOutput = rotL.process(rotL_internalInput);
-				char rotL_sealevelOutput = (char) ('A' + 
-						((rotL_internalOutput - 'A') + dispRotorInternalAboveSea) % 26);
-				
-				
+				for (int rot : new int[]{RIGHT, MID, LEFT}) {
+					
+					offsetRotInternalAboveSea = Utility.GetOffsetRotorInternal(ringSetting, windowSetting, rot);
+					inputValueRotorInternal = ((result - 'A') - offsetRotInternalAboveSea + 26) % 26;
+					
+					char currentRot_internalInput = (char) ('A' + inputValueRotorInternal);	
+					char currentRot_internalOutput = rotors[rot].process(currentRot_internalInput);
+					char currentRot_sealevelOutput = (char) ('A' + 
+							((currentRot_internalOutput - 'A') + offsetRotInternalAboveSea) % 26);
+					result = currentRot_sealevelOutput;
+					
+				}
 				
 
 				/* ***********************
 				 * REFLECTOR PROCESSING
 				 * ***********************/
 				
-				char ref_out = reflector.reflect(rotL_sealevelOutput);
+				result = reflector.reflect(result);
 
 				
 				/* ***********************
 				 * ROTOR IN REVERSE
 				 * ***********************/
 				
-				// LEFT ROTOR REVERSE
-				dispRingAboveSea = 'A' - windowSetting.getWindowValueLeft();
-				dispRotorInternalWrtRing = ringSetting.leftOffset();
-				dispRotorInternalAboveSea = (dispRingAboveSea + dispRotorInternalWrtRing + 26) % 26;
-				inputValueRotorInternal = ((ref_out - 'A') - dispRotorInternalAboveSea + 26) % 26;
-				char rev_rotL_internalInput = (char) ('A' + inputValueRotorInternal);
-				char rev_rotL_internalOutput = rotL.revProcess(rev_rotL_internalInput);
-				char rev_rotL_sealevelOutput = (char) ('A' + 
-						((rev_rotL_internalOutput - 'A') + dispRotorInternalAboveSea) % 26);
 				
-				// MIDDLE ROTOR REVERSE
-				dispRingAboveSea = 'A' - windowSetting.getWindowValueMid();
-				dispRotorInternalWrtRing = ringSetting.midOffset();
-				dispRotorInternalAboveSea = (dispRingAboveSea + dispRotorInternalWrtRing + 26) % 26;
-				inputValueRotorInternal = ((rev_rotL_sealevelOutput - 'A') - dispRotorInternalAboveSea + 26) % 26;
-				char rev_rotM_internalInput = (char) ('A' + inputValueRotorInternal);
-				char rev_rotM_internalOutput = rotM.revProcess(rev_rotM_internalInput);
-				char rev_rotM_sealevelOutput = (char) ('A' + 
-						((rev_rotM_internalOutput - 'A') + dispRotorInternalAboveSea) % 26);
+				for (int rot : new int[]{LEFT, MID, RIGHT}) {
+					
+					offsetRotInternalAboveSea = Utility.GetOffsetRotorInternal(ringSetting, windowSetting, rot);
+					inputValueRotorInternal = ((result - 'A') - offsetRotInternalAboveSea + 26) % 26;
+					
+					char currentRot_internalInput = (char) ('A' + inputValueRotorInternal);	
+					char currentRot_internalOutput = rotors[rot].revProcess(currentRot_internalInput);
+					char currentRot_sealevelOutput = (char) ('A' + 
+							((currentRot_internalOutput - 'A') + offsetRotInternalAboveSea) % 26);
+					result = currentRot_sealevelOutput;
+					
+				}
 				
 				
-				// RIGHT ROTOR REVERSE
-				dispRingAboveSea = 'A' - windowSetting.getWindowValueRight();
-				dispRotorInternalWrtRing = ringSetting.rightOffset();
-				dispRotorInternalAboveSea = (dispRingAboveSea + dispRotorInternalWrtRing + 26) % 26;
-				inputValueRotorInternal = ((rev_rotM_sealevelOutput - 'A') - dispRotorInternalAboveSea + 26) % 26;
-				char rev_rotR_internalInput = (char) ('A' + inputValueRotorInternal);
-				char rev_rotR_internalOutput = rotR.revProcess(rev_rotR_internalInput);
-				char rev_rotR_sealevelOutput = (char) ('A' + 
-						((rev_rotR_internalOutput - 'A') + dispRotorInternalAboveSea) % 26);
 				
 				
 				/* ***********************
 				 * PLUGBOARD TRANSLATION
 				 * ***********************/
 				
-				char final_out = plugboard.translate(rev_rotR_sealevelOutput);
+				char final_out = plugboard.translate(result);
 
 				buf.append(final_out);
 
